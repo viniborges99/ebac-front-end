c5e2ca8b55f5f1639da801cd6a7614b413906a8c
frontendebac <borgesvinicius27@gmail.com>
2022-01-09 14:52:08 -0300

    modulo 12
    adicionei um novo modulo

---
+++ exercicio modulo 13/script.js
@@ -0,0 +1,10 @@
+function calcular(){
+    var a = parseInt(document.getElementById("a").value);
+    var b = parseInt(document.getElementById("b").value);
+    var c = parseInt(document.getElementById("c").value);
+    
+    var media = (a+ b + c) / 3;
+    
+    document.getElementById("resultado").innerHTML= "resultado:" + media;
+    
+}
