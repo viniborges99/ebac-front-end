16937fce036a8e1c7dd11343fbacb632f6fdbadc
frontendebac <borgesvinicius27@gmail.com>
2022-03-07 21:02:55 -0300

    ajustes nos links
    ajustes nos links

--- Jquery/js/custom.js
+++ Jquery/js/custom.js
@@ -17,9 +17,64 @@ $(document). ready(function(){
 
     $('.featured-item a').on('blur', function(event){
         event.preventDefault();
-        alert('produto esgotado');
+        console.log('produto esgotado');
 
     })
-    
+    /*callback
+    são ações que começam ao termino de outra.
+    */
+    $('.featured-item:nth(1)').hide(2000, function(){
+        //esta linha de cima e o callback, ele ira ocultar o primeiro elemento ao terminar de ocultar ele vai receber um alert que e a linha de ha baixo.
+        console.log($(this).find('h4').text() + 'esgotado')
+    })
+    .show(2000, function(){
+        console.log($(this).find('h4').text() + 'em estoque ')
+    })
+
+    /*animaçoes*/
+    const duracao = 1000
+    $('.featured-item:nth(0)')
+    .hide(duracao)
+    .show(duracao)
+    .fadeOut(duracao)
+    .fadeIn(duracao)
+    .toggle(duracao)
+    .toggle(duracao)
+
+    $('#form-submit').on('click', function(e){
+        e.preventDefault()
+
+        if($('#email').val() != ""){
+            $('#email').animate({
+                opacity: "toggle",
+                top: "-50"
+            }, 500, function(){
+                console.log($(this).val())
+            })
+        }
+
+
+        if($('#email').val().length < 1){
+            $('#email').animate({
+                'border': '2px solid red'
+            })
+
+        }
+    })
+
+    //ouvinte de eventos .nav-modal-open
+
+    $('.nav-modal-open').on('click', function(e){
+        e.preventDefault();
+
+        let elem=$(this).attr('rel')
+
+        $('.modal-body').html($('#'+elem).html())
+        $('.modal-header h5.modal-title').html($(this).text())
+
+        let myModal = new bootstrap.Modal($('#modelId'))
+
+        myModal.show()
+    })
 
 })
