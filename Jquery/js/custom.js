35af7c035a2875ab74b64d503ade054aef900c91
frontendebac <borgesvinicius27@gmail.com>
2022-03-17 22:30:48 -0300

    melhorias
    melhorias e complementações de codigos

--- Jquery/js/custom.js
+++ Jquery/js/custom.js
@@ -77,4 +77,126 @@ $(document). ready(function(){
         myModal.show()
     })
 
+    function validate(elem){
+        if( elem.val() == ""){
+            console.log('os campos de estão vazios')
+
+            elem.parent().find('.text-muted').show()
+
+            elem.addClass('invalid')
+            return false
+        } else{
+            elem.parent().find('.text-muted').hide()
+
+            elem.removeClass('invalid')
+        }
+    }
+    
+
+    $('body').on('submit','.modal-body .form', function(e){
+
+        e.preventDefault()
+
+        const inputName = $("#nome")
+        const inputEmail = $("#email")
+
+        validate(inputName)
+        validate(inputEmail)
+
+        if(inputEmail.hasClass('invalid') || inputName.hasClass('invalid')){
+            return false
+        } else{
+           $(this).submit() 
+        }
+        
+    })
+   
+
+    $('body').on('blur', '#nome', function(){
+        validate($(this))
+    })
+
+    $('body').on('blur', '#email', function(){
+        validate($(this))
+    })
+
+    $('body').on("focus", '#date', function(){
+        $(this).datepicker();
+    })
+
+    $('body').on('blur', '#date', function(){
+        validate($(this))
+        $('#date').mask('00/00/0000');
+    })
+    $('body').on('blur', '#time', function(){
+        validate($(this))
+        $('#time').mask('00:00');
+    })
+    $('body').on('blur', '#cep', function(){
+        validate($(this))
+        $('#cep').mask('00000-000');
+    })
+    $('body').on('blur', '#phone', function(){
+        validate($(this))
+        $('#phone').mask('0000-0000');
+    })
+    $('body').on('blur', '#cpf', function(){
+        validate($(this))
+        $('#cpf').mask('000.000.000-00');
+        
+    })
+    function checarEmail(){
+        if( document.forms[0].email.value=="" 
+           || document.forms[0].email.value.indexOf('@')==-1 
+             || document.forms[0].email.value.indexOf('.')==-1 )
+            {
+              alert( "Por favor, informe um E-MAIL válido!" );
+              return false;
+            }
+    }
+
+
+    $(function(){
+    //Executa a requisição quando o campo username perder o foco
+    $('#cpf').blur(function()
+    {
+        var cpf = $('#cpf').val().replace(/[^0-9]/g, '').toString();
+
+        if( cpf.length == 11 )
+        {
+            var v = [];
+
+            //Calcula o primeiro dígito de verificação.
+            v[0] = 1 * cpf[0] + 2 * cpf[1] + 3 * cpf[2];
+            v[0] += 4 * cpf[3] + 5 * cpf[4] + 6 * cpf[5];
+            v[0] += 7 * cpf[6] + 8 * cpf[7] + 9 * cpf[8];
+            v[0] = v[0] % 11;
+            v[0] = v[0] % 10;
+
+            //Calcula o segundo dígito de verificação.
+            v[1] = 1 * cpf[1] + 2 * cpf[2] + 3 * cpf[3];
+            v[1] += 4 * cpf[4] + 5 * cpf[5] + 6 * cpf[6];
+            v[1] += 7 * cpf[7] + 8 * cpf[8] + 9 * v[0];
+            v[1] = v[1] % 11;
+            v[1] = v[1] % 10;
+
+            //Retorna Verdadeiro se os dígitos de verificação são os esperados.
+            if ( (v[0] != cpf[9]) || (v[1] != cpf[10]) )
+            {
+                alert('CPF inválido: ' + cpf);
+
+                $('#cpf').val('');
+                $('#cpf').focus();
+            }
+        }
+        else
+        {
+            alert('CPF inválido:' + cpf);
+
+            $('#cpf').val('');
+            $('#cpf').focus();
+        }
+    });
+});
+    
 })
