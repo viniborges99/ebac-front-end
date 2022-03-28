2747486816ca8edf0a9cacecb603492e3c2ffcd5
frontendebac <borgesvinicius27@gmail.com>
2022-03-28 20:29:46 -0300

    gulp

--- Gulp/projeto/gulp-gulp/gulpfile.js
+++ Gulp/projeto/gulp-gulp/gulpfile.js
@@ -93,8 +93,13 @@ gulp.task('serve', function(){
 
 })
 
+function end (cb){
+    console.log("tarefas concluidas")
+    return cb()
+}
+
 // series x parallel
-const process = series( tarefasHTML, tarefasJS, tarefasCSS)
+const process = series( tarefasHTML, tarefasJS, tarefasCSS, end)
 
 exports.styles = tarefasCSS
 exports.scripts = tarefasJS
