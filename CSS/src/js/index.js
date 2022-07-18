2689e5aeedc5912abc253c2ec172d9cd2a1702f7
frontendebac <borgesvinicius27@gmail.com>
2022-07-18 19:19:30 -0300

    flex box
    flex box

---
+++ CSS/src/js/index.js
@@ -0,0 +1,15 @@
+import '../scss/styles.scss'
+import imageBuilding from '../assets/building.jpg'
+import imageNotebook from '../assets/notebook.jpg'//importando a imagem
+
+const elemRoot = document.getElementById('root')
+elemRoot.classList.add('container')
+
+const elemImageBuilding = new Image()//estanciando a image, falando que tem uma nova img
+elemImageBuilding.src = imageBuilding// mostrando o src da img para identificar
+
+const elemNotebook = new Image()
+elemNotebook.src = imageNotebook
+
+document.querySelector('#building').appendChild(elemImageBuilding)//colocando a img no html
+document.querySelector('#notebook').appendChild(elemNotebook)
